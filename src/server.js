f23928ba91a4210b4060f63dff8a95a6e8167936
nostam <matsonho@gmail.com>
2021-01-15 15:15:25 +0100

    fix /products/productId/reviews error

--- src/server.js
+++ src/server.js
@@ -1,46 +1,49 @@
-const express = require("express")
-const listEndPoints = require("express-list-endpoints")
-const { join } = require("path")
-const cors = require("cors")
-const mongoose = require("mongoose")
+const express = require("express");
+const listEndPoints = require("express-list-endpoints");
+const { join } = require("path");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-const productsRouter = require("./services/products")
-const reviewsRouter = require("./services/reviews")
-const usersRouter = require("./services/users")
+const productsRouter = require("./services/products");
+const reviewsRouter = require("./services/reviews");
+const usersRouter = require("./services/users");
 
-const {notFoundHandler,
+const {
+  notFoundHandler,
   unauthorizedHandler,
   forbiddenHandler,
   badRequestHandler,
-  catchAllHandler} = require ("./lib/errorHandlers")
-const { ServerResponse } = require("http")
+  catchAllHandler,
+} = require("./lib/errorHandlers");
+const helmet = require("helmet");
 
-
-const server = express()
-const port = process.env.PORT
+const server = express();
+const port = process.env.PORT;
 
 /* const publiceFolderPath = join(__dirname, "../public")
 server.use(express.static(publiceFolderPath)) */
 
 const loggerMiddleware = (req, res, next) => {
-    console.log(`${req.url} ${req.method} -- ${new Date()}`)
-next()}
-//server.use(cors())
-server.use(express.json())
-server.use(loggerMiddleware)
+  console.log(`${req.url} ${req.method} -- ${new Date()}`);
+  next();
+};
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+server.use(loggerMiddleware);
 //Routes
-server.use("/products", productsRouter)
-server.use("/reviews", reviewsRouter)
-server.use("/users", usersRouter)
+server.use("/products", productsRouter);
+server.use("/reviews", reviewsRouter);
+server.use("/users", usersRouter);
 
 //Error Handlers
-server.use(notFoundHandler)
-server.use(unauthorizedHandler)
-server.use(forbiddenHandler)
-server.use(badRequestHandler)
-server.use(catchAllHandler)
+server.use(notFoundHandler);
+server.use(unauthorizedHandler);
+server.use(forbiddenHandler);
+server.use(badRequestHandler);
+server.use(catchAllHandler);
 
-console.log(listEndPoints(server))
+console.log(listEndPoints(server));
 
 /* mongoose.connect(process.env.MONGO_CONNECTION, {
     useNewUrlParser: true,
@@ -51,8 +54,15 @@ console.log(listEndPoints(server))
     }))
 .catch(error => console.log(error)) */
 
-mongoose.connect(process.env.MONGO_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
-    .then(server.listen(port, () => {
-    console.log("Server running on port", port)
-    }))
-.catch(err => console.log(err))
+mongoose
+  .connect(process.env.MONGO_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(
+    server.listen(port, () => {
+      console.log("Server running on port", port);
+    })
+  )
+  .catch((err) => console.log(err));
